b9e9b30b3482f9a8c85386a7f38c8cf7a8a6a7c0
wenyi616 <wenyiccchu@gmail.com>
2020-05-01 11:32:56 -0400

    go scale script

--- process-sql.py
+++ process-sql.py
@@ -24,65 +24,93 @@ def findIdx(df, colname, keyword):
         if web.rfind(keyword) != -1:
             return i
 
-for exp_number in range(1, 4):
-    exp_path = "./raw-data/exp" + str(exp_number) + "/crawl-data.sqlite"
 
+for exp_number in range(1,2):
+    exp_path = "./raw-data/exp"+str(exp_number)+"/crawl-data.sqlite"
+    
     conn = sqlite3.connect(exp_path)
-    javascript_cookies = pd.read_sql_query(
-        "select visit_id, record_type, change_cause, host, name, value, time_stamp from javascript_cookies;", conn)
-    http_responses = pd.read_sql_query(
-        "select visit_id, url, method, headers, time_stamp from http_responses where response_status == 200;", conn)
+    javascript_cookies = pd.read_sql_query("select visit_id, record_type, change_cause, host, name, value, time_stamp from javascript_cookies;", conn)
     http_requests = pd.read_sql_query("select visit_id, headers, referrer, Url, time_stamp from http_requests;", conn)
-    javascript = pd.read_sql_query(
-        "select visit_id, script_url, top_level_url, symbol, operation, value, arguments, time_stamp from javascript;", conn)
+    javascript = pd.read_sql_query("select visit_id, script_url, top_level_url, symbol, operation, value, arguments, time_stamp from javascript;", conn)
     site_visits = pd.read_sql_query("select visit_id, site_url from site_visits;", conn)
-
+    
     conn.close()
 
     names = list(map(lambda x: x.split('.')[1], list(site_visits.site_url)))
-    websites = list(names[i] for i in [0, 5, 10])
+    first_context = list( names[i] for i in [0,25,50])
+    websites = list( name for name in names[::5])
 
     web_dict = {
-        "nytimes": 'nytimes|nyt',
-        "webmd": 'webmd',
-        "berkeley": 'berkeley'
+        'cnn':'cnn',
+        'nytimes':'nytimes|nyt',
+        'theguardian':'theguardian',
+        'washingtonpost':'washingtonpost',
+        'forbes':'forbes',
+        'webmd':'webmd',
+        'mayoclinic':'mayoclinic',
+        'myfitnesspal':'myfitnesspal',
+        'medscape':'medscape',
+        'verywellhealth':'verywellhealth',
+        'jhu':'jhu',
+        'mit':'mit',
+        'edx':'edx',
+        'harvard':'harvard',
+        'collegeboard':'collegeboard'
     }
 
-    indx1 = findIdx(javascript_cookies, 'host', websites[1])
-    indx2 = findIdx(javascript_cookies, 'host', websites[2])
-    javascript_cookies["context_id"] = [1] * (indx1)  + [2] * (indx2 - indx1) + [3] * (javascript_cookies.shape[0] - indx2)
+    firstparty_dict = {
+        1 : 'cnn|nytimes|nyt|theguardian|washingtonpost|forbes',
+        2 : 'webmd|mayoclinic|myfitnesspal|medscape|verywellhealth',
+        3 : 'jhu|mit|edx|harvard|collegeboard'
+    }
 
-    indx1 = findIdx(http_requests, 'url', websites[1])
-    indx2 = findIdx(http_requests,'url', websites[2])
-    http_requests["context_id"] = [1] * (indx1)  + [2] * (indx2 - indx1) + [3] * (http_requests.shape[0] - indx2)
+    context_dict = {
+        "cnn": 1,
+        "webmd": 2,
+        "jhu": 3
+    }
 
-    indx1 = findIdx(javascript, 'top_level_url', websites[1])
-    indx2 = findIdx(javascript, 'top_level_url', websites[2])
-    javascript["context_id"] = [1] * (indx1)  + [2] * (indx2 - indx1) + [3] * (javascript.shape[0] - indx2)
+    # fix overlap (context_id)
+    indx1 = findIdx(javascript_cookies, 'host', first_context[1])
+    indx2 = findIdx(javascript_cookies, 'host', first_context[2])
+    javascript_cookies["context_id"] = [context_dict[first_context[0]]] * (indx1) + [context_dict[first_context[1]]] * (indx2 - indx1) + [context_dict[first_context[2]]] * (javascript_cookies.shape[0] - indx2)
+
+    indx1 = findIdx(http_requests, 'url', first_context[1])
+    indx2 = findIdx(http_requests,'url', first_context[2])
+    http_requests["context_id"] = [context_dict[first_context[0]]] * (indx1) + [context_dict[first_context[1]]] * (indx2 - indx1) + [context_dict[first_context[2]]] * (http_requests.shape[0] - indx2)
+
+    indx1 = findIdx(javascript, 'top_level_url', first_context[1])
+    indx2 = findIdx(javascript, 'top_level_url', first_context[2])
+    javascript["context_id"] = [context_dict[first_context[0]]] * (indx1) + [context_dict[first_context[1]]] * (indx2 - indx1) + [context_dict[first_context[2]]] * (javascript.shape[0] - indx2)
+
+
     for context_number in range(1,4):
         
         ############################# cookies #############################
         js_cookies = javascript_cookies[javascript_cookies.context_id == context_number]
     
         # first party cookies (filter by host)
-        js_cookies_first = js_cookies[js_cookies.host.str.contains(web_dict.get(websites[context_number-1]))]
+        js_cookies_first = js_cookies[js_cookies.host.str.contains(firstparty_dict[context_number])]
 
         # third party cookies (filter by host)
-        js_cookies_third = js_cookies[~js_cookies.host.str.contains(web_dict.get(websites[context_number-1]))]
+        js_cookies_third = js_cookies[~js_cookies.host.str.contains(firstparty_dict[context_number])]
 
-        js_cookies_third.to_csv('tables/' + str(exp_number)+'-'+str(context_number)+'-ThirdCookies.csv', index=False)
+        js_cookies_third.to_csv('tables/' + str(exp_number) + '-' + str(context_number) + '-ThirdCookies.csv', index=False)
         
         
         ############################# JS #############################
         js = javascript[javascript.context_id == context_number]
         
-        # filter by script_url (get domain from script_url)
-        temp = list(map(lambda x:x[1], js['script_url'].str.split(".")))
-        js['temp'] = temp
+        # script_url might be empty
+        js.drop(js[js['script_url'] == ''].index, inplace = True)
         
-        js_third = js[~js.script_url.str.contains(web_dict.get(websites[context_number-1]))]
+        # filter by script_url (get domain from script_url)
+        temp = list(map(lambda x:".".join(x.split(".")[:2]).replace('https://','').replace('www',''), js['script_url'].tolist()))                    
+        js['script_domain'] = temp
+        
+        js_third = js[~js.script_url.str.contains(firstparty_dict[context_number])]
 
-        js_third.to_csv('tables/' + str(exp_number)+'-'+str(context_number)+'-js.csv', index=False)
+        js_third.to_csv('tables/' + str(exp_number) + '-' + str(context_number) + '-js.csv', index=False)
         
         
         ############################# HTTP REQUEST #############################
@@ -95,11 +123,11 @@ for exp_number in range(1, 4):
         # get domain from host (eg. "ade.googlesyndication.com" -> "googlesyndication")
         hr = http_requests[http_requests.context_id == context_number]
 
-        temp = list(map(lambda x:x[-2], hr['host'].str.split(".")))
-        hr['temp'] = temp
+        hr.drop(hr[hr['host'] == ''].index, inplace = True)
+        temp = list(map(lambda x:x.split(".")[-2] if len(x.split(".")) > 1 else x, hr['host'].tolist()))
         
-        hr_third = hr[~hr.temp.str.contains(web_dict.get(websites[context_number-1]))]
+        hr['host_domain'] = temp
+        
+        hr_third = hr[~hr.host_domain.str.contains(firstparty_dict[context_number])]
 
-        hr_third.to_csv('tables/' + str(exp_number)+'-'+str(context_number)+'-httprequest.csv', index=False)
-
-
+        hr_third.to_csv('tables/' + str(exp_number) + '-' + str(context_number) + '-httprequest.csv', index=False)
